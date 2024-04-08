54803081f84d7d958cc623d0ce0605c01651ca09
SergeyMankevich <this_mail_for_everything@mail.ru>
2024-04-08 23:05:19 +0300

    create calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './app.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const data = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+	const [numbers] = useState(data);
+	const [inputValue, setInputValue] = useState('');
+	const [isEqualPressed, setIsEqualPressed] = useState(false); // для отображение стилей, при выводе результата
 
-export default App;
+	const reset = () => {
+		setInputValue('');
+		setIsEqualPressed(false);
+	};
+
+	const sum = () => operation('+');
+	const minus = () => operation('-');
+
+	const operation = (operator) => {
+		const lastChar = inputValue.slice(-1);
+		if (isNaN(Number(lastChar))) {
+			// чтобы нельзя было поставить много + или -, и возможно было заменить один знак на другой
+			setInputValue(inputValue.slice(0, -1) + operator);
+			setIsEqualPressed(false);
+		} else {
+			setInputValue(inputValue + operator);
+		}
+	};
+	const findResult = () => {
+		if (inputValue.length === 1 && isNaN(Number(inputValue[0]))) {
+			// если в строке ввода, тольк знак операции
+			reset();
+			return;
+		}
+		if (inputValue) {
+			let lastChar = inputValue.slice(-1);
+			let res;
+			if (isNaN(parseInt(lastChar))) {
+				res = eval(inputValue.slice(0, -1));
+			} else {
+				res = eval(inputValue);
+			}
+			setInputValue(res.toString());
+			setIsEqualPressed(true);
+		}
+	};
+
+	return (
+		<div className={styles.container}>
+			<div
+				className={`${styles.inputResult} ${isEqualPressed ? styles.resultColor : ''}`}
+			>
+				{inputValue}
+			</div>
+			<div className={styles.mainContent}>
+				<div className={styles.digitContainer}>
+					{numbers.map((digit, index) => (
+						<button
+							key={index}
+							className={styles.digit}
+							onClick={() => {
+								setInputValue(inputValue + String(digit));
+							}}
+						>
+							{digit}
+						</button>
+					))}
+				</div>
+				<div className={styles.functionContainer}>
+					<button className={styles.func} onClick={reset}>
+						c
+					</button>
+					<button className={styles.func} onClick={sum}>
+						+
+					</button>
+					<button className={styles.func} onClick={minus}>
+						-
+					</button>
+					<button className={styles.func} onClick={findResult}>
+						=
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
